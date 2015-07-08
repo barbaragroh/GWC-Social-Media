d934ea5b91aafedb58573c543e0f9c7d75c4fe08
barbaragroh <bgroh1997@gmail.com>
2015-07-08 14:51:05 -0400

    gooo

--- hashtagscode.js
+++ hashtagscode.js
@@ -10,8 +10,8 @@ $(document).ready(function () {
 		$("#gciframe").attr("src","http://widget.websta.me/in/tag:girlscan/?s=100&w=2&h=5&b=0&p=10");
 		$("#ciframe").attr("src","http://widget.websta.me/in/tag:coding/?s=100&w=2&h=5&b=0&p=10");
 		$("#gitiframe").attr("src","http://widget.websta.me/in/tag:girlsintech/?s=100&w=2&h=5&b=0&p=10");
-		$(".hashtag").css("height","525px");
-		$(".hashtag").css("width","50px");
+		$(".embed-responsive-item").css("height","525px");
+		$(".embed-responsive-item").css("width","50px");
 	}
 });
 $(window).resize(function() {
@@ -25,8 +25,8 @@ $(window).resize(function() {
 		$("#gciframe").attr("src","http://widget.websta.me/in/tag:girlscan/?s=100&w=2&h=5&b=0&p=10");
 		$("#ciframe").attr("src","http://widget.websta.me/in/tag:coding/?s=100&w=2&h=5&b=0&p=10");
 		$("#gitiframe").attr("src","http://widget.websta.me/in/tag:girlsintech/?s=100&w=2&h=5&b=0&p=10");
-		$(".hashtag").css("height","525px");
-		$(".hashtag").css("width","210px");
+		$(".embed-responsive-item").css("height","525px");
+		$(".embed-responsive-item").css("width","210px");
 	}
 	if($(window).width() > 800){// u can choose the size of the window also.
         $("#btgciframe").attr("src","http://widget.websta.me/in/tag:breaktheglassceiling/?s=200&w=7&h=10&b=0&p=10");
@@ -38,8 +38,8 @@ $(window).resize(function() {
 		$("#gciframe").attr("src","http://widget.websta.me/in/tag:girlscan/?s=200&w=7&h=10&b=0&p=10");
 		$("#ciframe").attr("src","http://widget.websta.me/in/tag:coding/?s=200&w=7&h=10&b=0&p=10");
 		$("#gitiframe").attr("src","http://widget.websta.me/in/tag:girlsintech/?s=200&w=7&h=10&b=0&p=10");
-		$(".hashtag").css("height","2100px");
-		$(".hashtag").css("width","2100px");
+		$(".embed-responsive-item").css("height","2100px");
+		$(".embed-responsive-item").css("width","2100px");
 	}
 	
 });
