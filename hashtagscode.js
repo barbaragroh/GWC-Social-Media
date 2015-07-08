f06bc338c1089babe865488dbce7e43b27a58a08
barbaragroh <bgroh1997@gmail.com>
2015-07-08 14:22:10 -0400

    Make Hashtags mobile
    getting there

--- hashtagscode.js
+++ hashtagscode.js
@@ -1,31 +1,45 @@
 $(document).ready(function () {
 
 	if($(window).width() <= 800){// u can choose the size of the window also.
-        $("btgciframe").attr("src","http://widget.websta.me/in/tag:breaktheglassceiling/?s=100&w=2&h=5&b=0&p=10");
-		$("gbtiframe").attr("src","http://widget.websta.me/in/tag:girlsbuiltthis/?s=100&w=2&h=5&b=0&p=10");
-		$("girlswciframe").attr("src","http://widget.websta.me/in/tag:girlswhocode/?s=100&w=2&h=5&b=0&p=10");
-		$("ctggiframe").attr("src","http://widget.websta.me/in/tag:closethegendergap/?s=100&w=2&h=5&b=0&p=10");
-		$("gwciframe").attr("src","http://widget.websta.me/in/tag:gwc/?s=100&w=2&h=5&b=0&p=10");
-		$("gwcciframe").attr("src","http://widget.websta.me/in/tag:gwcclubs/?s=100&w=2&h=5&b=0&p=10");
-		$("gciframe").attr("src","http://widget.websta.me/in/tag:girlscan/?s=100&w=2&h=5&b=0&p=10");
-		$("ciframe").attr("src","http://widget.websta.me/in/tag:coding/?s=100&w=2&h=5&b=0&p=10");
-		$("gitiframe").attr("src","http://widget.websta.me/in/tag:girlsintech/?s=100&w=2&h=5&b=0&p=10");
-
+        $("#btgciframe").attr("src","http://widget.websta.me/in/tag:breaktheglassceiling/?s=100&w=2&h=5&b=0&p=10");
+		$("#gbtiframe").attr("src","http://widget.websta.me/in/tag:girlsbuiltthis/?s=100&w=2&h=5&b=0&p=10");
+		$("#girlswciframe").attr("src","http://widget.websta.me/in/tag:girlswhocode/?s=100&w=2&h=5&b=0&p=10");
+		$("#ctggiframe").attr("src","http://widget.websta.me/in/tag:closethegendergap/?s=100&w=2&h=5&b=0&p=10");
+		$("#gwciframe").attr("src","http://widget.websta.me/in/tag:gwc/?s=100&w=2&h=5&b=0&p=10");
+		$("#gwcciframe").attr("src","http://widget.websta.me/in/tag:gwcclubs/?s=100&w=2&h=5&b=0&p=10");
+		$("#gciframe").attr("src","http://widget.websta.me/in/tag:girlscan/?s=100&w=2&h=5&b=0&p=10");
+		$("#ciframe").attr("src","http://widget.websta.me/in/tag:coding/?s=100&w=2&h=5&b=0&p=10");
+		$("#gitiframe").attr("src","http://widget.websta.me/in/tag:girlsintech/?s=100&w=2&h=5&b=0&p=10");
+		/*$(".hashtag").css("height","525px");
+		$(".hashtag").css("width","50px");*/
 	}
-	
 });
 $(window).resize(function() {
      if($(window).width() <= 800){// u can choose the size of the window also.
-        $("btgciframe").attr("src","http://widget.websta.me/in/tag:breaktheglassceiling/?s=100&w=2&h=5&b=0&p=10");
-		$("gbtiframe").attr("src","http://widget.websta.me/in/tag:girlsbuiltthis/?s=100&w=2&h=5&b=0&p=10");
-		$("girlswciframe").attr("src","http://widget.websta.me/in/tag:girlswhocode/?s=100&w=2&h=5&b=0&p=10");
-		$("ctggiframe").attr("src","http://widget.websta.me/in/tag:closethegendergap/?s=100&w=2&h=5&b=0&p=10");
-		$("gwciframe").attr("src","http://widget.websta.me/in/tag:gwc/?s=100&w=2&h=5&b=0&p=10");
-		$("gwcciframe").attr("src","http://widget.websta.me/in/tag:gwcclubs/?s=100&w=2&h=5&b=0&p=10");
-		$("gciframe").attr("src","http://widget.websta.me/in/tag:girlscan/?s=100&w=2&h=5&b=0&p=10");
-		$("ciframe").attr("src","http://widget.websta.me/in/tag:coding/?s=100&w=2&h=5&b=0&p=10");
-		$("gitiframe").attr("src","http://widget.websta.me/in/tag:girlsintech/?s=100&w=2&h=5&b=0&p=10");
-
+        $("#btgciframe").attr("src","http://widget.websta.me/in/tag:breaktheglassceiling/?s=100&w=2&h=5&b=0&p=10");
+		$("#gbtiframe").attr("src","http://widget.websta.me/in/tag:girlsbuiltthis/?s=100&w=2&h=5&b=0&p=10");
+		$("#girlswciframe").attr("src","http://widget.websta.me/in/tag:girlswhocode/?s=100&w=2&h=5&b=0&p=10");
+		$("#ctggiframe").attr("src","http://widget.websta.me/in/tag:closethegendergap/?s=100&w=2&h=5&b=0&p=10");
+		$("#gwciframe").attr("src","http://widget.websta.me/in/tag:gwc/?s=100&w=2&h=5&b=0&p=10");
+		$("#gwcciframe").attr("src","http://widget.websta.me/in/tag:gwcclubs/?s=100&w=2&h=5&b=0&p=10");
+		$("#gciframe").attr("src","http://widget.websta.me/in/tag:girlscan/?s=100&w=2&h=5&b=0&p=10");
+		$("#ciframe").attr("src","http://widget.websta.me/in/tag:coding/?s=100&w=2&h=5&b=0&p=10");
+		$("#gitiframe").attr("src","http://widget.websta.me/in/tag:girlsintech/?s=100&w=2&h=5&b=0&p=10");
+		/*$(".hashtag").css("height","525px");
+		$(".hashtag").css("width","210px");*/
+	}
+	if($(window).width() > 800){// u can choose the size of the window also.
+        $("#btgciframe").attr("src","http://widget.websta.me/in/tag:breaktheglassceiling/?s=200&w=7&h=10&b=0&p=10");
+		$("#gbtiframe").attr("src","http://widget.websta.me/in/tag:girlsbuiltthis/?s=200&w=7&h=10&b=0&p=10");
+		$("#girlswciframe").attr("src","http://widget.websta.me/in/tag:girlswhocode/?s=200&w=7&h=10&b=0&p=10");
+		$("#ctggiframe").attr("src","http://widget.websta.me/in/tag:closethegendergap/?s=200&w=7&h=10&b=0&p=10");
+		$("#gwciframe").attr("src","http://widget.websta.me/in/tag:gwc/?s=200&w=7&h=10&b=0&p=10");
+		$("#gwcciframe").attr("src","http://widget.websta.me/in/tag:gwcclubs/?s=200&w=7&h=10&b=0&p=10");
+		$("#gciframe").attr("src","http://widget.websta.me/in/tag:girlscan/?s=200&w=7&h=10&b=0&p=10");
+		$("#ciframe").attr("src","http://widget.websta.me/in/tag:coding/?s=200&w=7&h=10&b=0&p=10");
+		$("#gitiframe").attr("src","http://widget.websta.me/in/tag:girlsintech/?s=200&w=7&h=10&b=0&p=10");
+		$(".hashtag").css("height","2100px");
+		$(".hashtag").css("width","2100px");
 	}
 	
 });
