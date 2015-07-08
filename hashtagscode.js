859792a68906034a7810f82cd958224d5fc0c28b
barbaragroh <bgroh1997@gmail.com>
2015-07-08 14:26:08 -0400

    ggo

--- hashtagscode.js
+++ hashtagscode.js
@@ -10,8 +10,8 @@ $(document).ready(function () {
 		$("#gciframe").attr("src","http://widget.websta.me/in/tag:girlscan/?s=100&w=2&h=5&b=0&p=10");
 		$("#ciframe").attr("src","http://widget.websta.me/in/tag:coding/?s=100&w=2&h=5&b=0&p=10");
 		$("#gitiframe").attr("src","http://widget.websta.me/in/tag:girlsintech/?s=100&w=2&h=5&b=0&p=10");
-		/*$(".hashtag").css("height","525px");
-		$(".hashtag").css("width","50px");*/
+		$(".hashtag").css("height","525px");
+		$(".hashtag").css("width","50px");
 	}
 });
 $(window).resize(function() {
@@ -25,8 +25,8 @@ $(window).resize(function() {
 		$("#gciframe").attr("src","http://widget.websta.me/in/tag:girlscan/?s=100&w=2&h=5&b=0&p=10");
 		$("#ciframe").attr("src","http://widget.websta.me/in/tag:coding/?s=100&w=2&h=5&b=0&p=10");
 		$("#gitiframe").attr("src","http://widget.websta.me/in/tag:girlsintech/?s=100&w=2&h=5&b=0&p=10");
-		/*$(".hashtag").css("height","525px");
-		$(".hashtag").css("width","210px");*/
+		$(".hashtag").css("height","525px");
+		$(".hashtag").css("width","210px");
 	}
 	if($(window).width() > 800){// u can choose the size of the window also.
         $("#btgciframe").attr("src","http://widget.websta.me/in/tag:breaktheglassceiling/?s=200&w=7&h=10&b=0&p=10");
